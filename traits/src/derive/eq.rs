ac028dec61b0ea9ea6e7d91c36ca4d4c0755dce3
Your Name <you@example.com>
2021-08-18 11:55:27 +0900

    Fix derive eq

--- traits/src/derive/eq.rs
+++ traits/src/derive/eq.rs
@@ -1,13 +1,12 @@
+//! # Eq
+//!
+//! PartialEqを実装していれば実装できるというわけではない
+//!
+//! PartialEqも実装する必要がある
+//!
+//! フィールドもEqを実装する必要がある
+
 #[derive(Debug, Eq, PartialEq)]
 struct Struct;
 
-pub fn main() {
-    {
-        // Eq
-        // PartialEqを実装していれば実装できるというわけではない
-        // PartialEqも実装する必要がある
-        // フィールドもEqを実装する必要がある
-
-        // TODO
-    }
-}
+pub fn main() {}

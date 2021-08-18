348847680ffb890341fb09fe4be2593cc38f2437
Your Name <you@example.com>
2021-08-18 10:18:14 +0900

    Add eq to derive

---
+++ traits/src/derive/eq.rs
@@ -0,0 +1,13 @@
+#[derive(Debug, Eq, PartialEq)]
+struct Struct;
+
+pub fn main() {
+    {
+        // Eq
+        // PartialEqを実装していれば実装できるというわけではない
+        // PartialEqも実装する必要がある
+        // フィールドもEqを実装する必要がある
+
+        // TODO
+    }
+}
--- traits/src/derive/mod.rs
+++ traits/src/derive/mod.rs
@@ -1,7 +1,9 @@
 mod debug;
+mod eq;
 mod partial_eq;
 
 pub fn main() {
     debug::main();
     partial_eq::main();
+    eq::main();
 }

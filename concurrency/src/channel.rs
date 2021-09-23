5c85386fd466de4f7ef6253c0070f047691ae240
emgniddikur <yoshiharu2580@gmail.com>
2021-09-23 18:37:00 +0900

    Add clone_tx to concurrency channel

--- concurrency/src/channel.rs
+++ concurrency/src/channel.rs
@@ -30,8 +30,38 @@ fn multiple() {
     }
 }
 
+fn clone_tx() {
+    let (tx, rx) = mpsc::channel();
+
+    let tx1 = mpsc::Sender::clone(&tx);
+
+    thread::spawn(move || {
+        let nums = vec![1, 2, 3];
+        for num in nums {
+            tx1.send(num).unwrap();
+
+            thread::sleep(Duration::from_secs(1));
+        }
+    });
+
+    thread::spawn(move || {
+        let nums = vec![4, 5, 6];
+        for num in nums {
+            tx.send(num).unwrap();
+
+            thread::sleep(Duration::from_secs(1));
+        }
+    });
+
+    for received in rx {
+        println!("{}", received);
+    }
+}
+
 pub fn main() {
     single();
 
     multiple();
+
+    clone_tx();
 }

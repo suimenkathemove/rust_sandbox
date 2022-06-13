4a0ac3a1bdd73d81a56053cebb762bd3731082be
emgniddikur <yoshiharu2580@gmail.com>
2022-06-14 01:30:11 +0900

    add call macro

--- macros/src/main.rs
+++ macros/src/main.rs
@@ -1,9 +1,22 @@
+use std::fmt::Display;
+
 macro_rules! add {
     ($e1: expr, $e2: expr) => {
         $e1 + $e2
     };
 }
 
+macro_rules! call {
+    ($func: ident, $e: expr) => {
+        $func($e)
+    };
+}
+
+fn my_print<T: Display>(arg: T) {
+    println!("{}", arg);
+}
+
 fn main() {
     println!("{}", add!(1, 2));
+    call!(my_print, 3);
 }

5187a709d3b798df3b70c06669e0bde65f603326
emgniddikur <yoshiharu2580@gmail.com>
2022-07-30 00:17:52 +0900

    add DivError

--- error_handling/src/main.rs
+++ error_handling/src/main.rs
@@ -1,3 +1,37 @@
+enum DivError {
+    DivByZero(i32),
+}
+
+fn div(x: i32, y: i32) -> Result<i32, DivError> {
+    if y == 0 {
+        return Err(DivError::DivByZero(x));
+    }
+
+    Ok(x / y)
+}
+
+fn display_result<T: std::fmt::Display, E: std::fmt::Display>(result: Result<T, E>) {
+    match result {
+        Ok(v) => {
+            println!("{}", v);
+        }
+        Err(e) => {
+            println!("{}", e);
+        }
+    }
+}
+
 fn main() {
-    println!("Hello, world!");
+    // matchで場合分けする
+
+    // 起こり得るエラーの種類を網羅した列挙型を定義する
+
+    match div(2, 2) {
+        Ok(v) => {
+            println!("{}", v);
+        }
+        Err(DivError::DivByZero(x)) => {
+            println!("{}", x);
+        }
+    }
 }

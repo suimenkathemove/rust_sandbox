60483a675c706d122c796d93c06ac1974f71a4d9
emgniddikur <yoshiharu2580@gmail.com>
2022-07-30 01:26:06 +0900

    add error_propagation

--- error_handling/src/error_propagation.rs
+++ error_handling/src/error_propagation.rs
@@ -1,1 +1,25 @@
-pub fn main() {}
+use thiserror::Error;
+
+#[derive(Error, Debug)]
+enum Error {
+    #[error("error")]
+    Variant(i32),
+}
+
+fn func(arg: i32) -> Result<i32, Error> {
+    if arg % 2 == 0 {
+        Ok(arg)
+    } else {
+        Err(Error::Variant(arg))
+    }
+}
+
+fn exec_func(arg: i32) -> Result<(), Error> {
+    println!("result: {}", func(arg)?);
+    Ok(())
+}
+
+pub fn main() {
+    exec_func(2).ok();
+    println!("{}", exec_func(1).err().unwrap());
+}

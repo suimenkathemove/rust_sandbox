c414a1151428c3b1e6b278def5131367d71b8810
emgniddikur <yoshiharu2580@gmail.com>
2022-10-31 12:56:57 +0900

    add option_ref_to_option

--- type_conversion/src/main.rs
+++ type_conversion/src/main.rs
@@ -1,3 +1,10 @@
-fn main() {
-    println!("Hello, world!");
+// Option<&T> -> Option<T>
+fn option_ref_to_option<T: Clone>(arg: Option<&T>) -> Option<T> {
+    arg.map(|a| a.clone())
 }
+fn option_ref_to_option2<T: Clone>(arg: Option<&T>) -> Option<T> {
+    let a = arg?;
+    Some(a.clone())
+}
+
+fn main() {}

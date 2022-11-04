147f8847319be9fab4453ea6c94e6a96d6ac68c3
emgniddikur <yoshiharu2580@gmail.com>
2022-11-04 19:52:47 +0900

    use parse_macro_input

--- macros/builder/src/lib.rs
+++ macros/builder/src/lib.rs
@@ -1,8 +1,8 @@
 use proc_macro::TokenStream;
+use syn::{parse_macro_input, DeriveInput};
 
 #[proc_macro_derive(Builder)]
 pub fn derive(input: TokenStream) -> TokenStream {
-    let _ = input;
-
+    let _input = parse_macro_input!(input as DeriveInput);
     TokenStream::new()
 }

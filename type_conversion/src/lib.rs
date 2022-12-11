4acbaddb247329917d420b662db1aa3c6ce89466
suimenkathemove <yoshiharu2580@gmail.com>
2022-12-11 13:24:49 +0900

    fix type_conversion to library crate

---
+++ type_conversion/src/lib.rs
@@ -0,0 +1,8 @@
+// Option<&T> -> Option<T>
+pub fn option_ref_to_option<T: Clone>(arg: Option<&T>) -> Option<T> {
+    arg.map(|a| a.clone())
+}
+pub fn option_ref_to_option2<T: Clone>(arg: Option<&T>) -> Option<T> {
+    let a = arg?;
+    Some(a.clone())
+}

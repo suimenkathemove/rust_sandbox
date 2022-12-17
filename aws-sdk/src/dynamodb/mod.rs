b738405a8eaddc2dcef9487f10d7d152a25420b8
suimenkathemove <yoshiharu2580@gmail.com>
2022-12-18 00:48:10 +0900

    move dotenv().ok()

--- aws-sdk/src/dynamodb/mod.rs
+++ aws-sdk/src/dynamodb/mod.rs
@@ -1,9 +1,6 @@
 use aws_sdk_dynamodb::{Client, Error};
-use dotenv::dotenv;
 
 pub async fn list_table_names() -> Result<(), Error> {
-    dotenv().ok();
-
     let shared_config = aws_config::load_from_env().await;
     let client = Client::new(&shared_config);
     let req = client.list_tables().limit(10);
--- aws-sdk/src/main.rs
+++ aws-sdk/src/main.rs
@@ -1,7 +1,12 @@
 mod dynamodb;
 
+use dotenv::dotenv;
+
 #[tokio::main]
 async fn main() -> Result<(), Box<dyn std::error::Error>> {
+    dotenv().ok();
+
     dynamodb::list_table_names().await?;
+
     Ok(())
 }

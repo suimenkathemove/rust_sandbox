6b260265512e7adc47ec3d6f5ea0ad9db2104348
suimenkathemove <yoshiharu2580@gmail.com>
2022-12-18 20:06:31 +0900

    fix

--- aws-sdk/src/main.rs
+++ aws-sdk/src/main.rs
@@ -2,8 +2,5 @@ mod dynamodb;
 
 #[tokio::main]
 async fn main() -> Result<(), Box<dyn std::error::Error>> {
-    // dynamodb::list_table_names().await?;
-    // dynamodb::get_item().await?;
-
     Ok(())
 }

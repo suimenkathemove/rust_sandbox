d9819f0835eaf7f6eb82e4320eac444444a5d036
suimenkathemove <yoshiharu2580@gmail.com>
2022-12-18 13:19:30 +0900

    rename to from_json

--- type_conversion/src/json.rs
+++ type_conversion/src/json.rs
@@ -1,7 +1,7 @@
 use serde::{de::Deserialize, Serialize};
 use serde_json::Error;
 
-pub fn json_to_deserialize<'a, T: Deserialize<'a>>(json: &'a str) -> Result<T, Error> {
+pub fn from_json<'a, T: Deserialize<'a>>(json: &'a str) -> Result<T, Error> {
     let value = serde_json::from_str(json)?;
     Ok(value)
 }
@@ -18,7 +18,7 @@ mod tests {
 
     #[test]
     fn json_to_hash_map() {
-        let expected: HashMap<String, String> = json_to_deserialize(r#"{ "foo": "bar" }"#).unwrap();
+        let expected: HashMap<String, String> = from_json(r#"{ "foo": "bar" }"#).unwrap();
         let actual = HashMap::from([("foo".to_string(), "bar".to_string())]);
         assert_eq!(expected, actual);
     }

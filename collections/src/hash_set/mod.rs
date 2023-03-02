1927c2f6c85d847e191802a8fba05bc66da39c28
suimenkathemove <yoshiharu2580@gmail.com>
2023-03-02 18:37:40 +0900

    add intersection to hash_set

--- collections/src/hash_set/mod.rs
+++ collections/src/hash_set/mod.rs
@@ -9,4 +9,12 @@ mod tests {
         set.insert(1);
         dbg!(set);
     }
+
+    #[test]
+    fn intersection() {
+        let set1 = HashSet::from([0, 1]);
+        let set2 = HashSet::from([1, 2]);
+        let set3 = set1.intersection(&set2).map(|&i| i).collect();
+        assert_eq!(HashSet::from([1]), set3);
+    }
 }

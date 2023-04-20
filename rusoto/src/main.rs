22dfe6f2c6759ba107cb5bfc076ea1419b842415
suimenkathemove <yoshiharu2580@gmail.com>
2023-04-20 11:17:13 +0900

    add rusoto_dynamodb

--- rusoto/src/main.rs
+++ rusoto/src/main.rs
@@ -1,3 +1,20 @@
-fn main() {
-    println!("Hello, world!");
+use rusoto_core::Region;
+use rusoto_dynamodb::{DynamoDb, DynamoDbClient};
+
+#[tokio::main]
+async fn main() -> anyhow::Result<()> {
+    let client = DynamoDbClient::new(Region::ApNortheast1);
+    let output = client.list_tables(Default::default()).await?;
+    match output.table_names {
+        Some(table_names) => {
+            table_names.iter().for_each(|table_name| {
+                dbg!(table_name);
+            });
+        }
+        None => {
+            println!("no tables");
+        }
+    }
+
+    Ok(())
 }

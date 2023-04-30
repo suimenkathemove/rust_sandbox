e185a37ff85d1aae43dc2c10b67339987a9fed57
suimenkathemove <yoshiharu2580@gmail.com>
2023-04-30 10:03:13 +0900

    add remove_attributes

--- rusoto/src/main.rs
+++ rusoto/src/main.rs
@@ -1,5 +1,32 @@
 use rusoto_core::Region;
-use rusoto_dynamodb::{DynamoDb, DynamoDbClient};
+use rusoto_dynamodb::{DynamoDb, DynamoDbClient, ScanInput, UpdateItemInput};
+use std::collections::HashMap;
+
+async fn remove_attributes(table_name: &str, attribute_names: Vec<&str>) -> anyhow::Result<()> {
+    let client = DynamoDbClient::new(Region::ApNortheast1);
+
+    let items = client
+        .scan(ScanInput {
+            table_name: table_name.to_string(),
+            ..Default::default()
+        })
+        .await?
+        .items
+        .expect("failed to get items");
+    for item in items.iter() {
+        let id = item.get("id").expect("failed to get id");
+        client
+            .update_item(UpdateItemInput {
+                table_name: table_name.to_string(),
+                key: HashMap::from([("id".to_string(), id.clone())]),
+                update_expression: Some(format!("REMOVE {}", attribute_names.join(", "))),
+                ..Default::default()
+            })
+            .await?;
+    }
+
+    Ok(())
+}
 
 #[tokio::main]
 async fn main() -> anyhow::Result<()> {

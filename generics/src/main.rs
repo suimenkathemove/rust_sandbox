4ccefb75456ab3fac93b1acbce814fea25d252ff
suimenkathemove <yoshiharu2580@gmail.com>
2023-09-09 14:53:09 +0900

    add to generics

--- generics/src/main.rs
+++ generics/src/main.rs
@@ -1,3 +1,9 @@
+//! Generics
+//!
+//! ジェネリクス（generics）には、ジェネリック型（generic type）とジェネリック関数（generic function）がある。
+//! ジェネリック型は、定数や型を受け取り、型を返す。
+//! ジェネリック関数は、定数や型を受け取り、関数を返す。
+
 #![allow(dead_code)]
 
 mod point;

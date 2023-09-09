34585470d0b9b839ab5c1cdef8a552e5a1369a90
suimenkathemove <yoshiharu2580@gmail.com>
2023-09-09 23:43:07 +0900

    add to collections

--- collections/src/main.rs
+++ collections/src/main.rs
@@ -1,3 +1,10 @@
+//! # Collections
+//!
+//! シーケンス（sequence）、マップ（map）、セット（set）のデータ構造を提供する
+//! シーケンスには、Vec、VecDeque、LinkedListがある
+//! マップには、HashMap、BTreeMapがある
+//! セットには、HashSet、BTreeSetがある
+
 mod b_tree_set;
 mod hash_set;
 

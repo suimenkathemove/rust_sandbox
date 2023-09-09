252e4a830c0ff9577f8b25d2a3e8ce3b7606e46e
suimenkathemove <yoshiharu2580@gmail.com>
2023-09-09 17:55:46 +0900

    add to closures

--- closures/src/capture_env.rs
+++ closures/src/capture_env.rs
@@ -1,13 +1,39 @@
+// クロージャの概念的な構造
+fn _closure_struct() {
+    // クロージャの環境
+    struct Env<'a, T> {
+        variable: &'a mut T,
+    }
+
+    // クロージャのファットポインタ
+    struct Closure<'a, T> {
+        ptr_func: fn(),
+        // キャプチャした変数を保持する領域を`環境`と呼ぶ
+        ptr_env: Box<Env<'a, T>>,
+    }
+}
+
 pub fn main() {
-    let n = 0;
+    {
+        let n = 0;
 
-    // 関数は環境をキャプチャできない
-    // fn function() -> i32 {
-    //     n
-    // }
-    // function();
+        // 関数は環境をキャプチャできない
+        // fn function() -> i32 {
+        //     n
+        // }
+        // function();
 
-    // クロージャは環境をキャプチャできる
-    let closure = || n;
-    closure();
+        // クロージャは環境をキャプチャできる
+        let closure = || n;
+        closure();
+    }
+
+    {
+        let mut n = 1;
+        // 環境に変更を加えるため、変数fもmutにしなければならない
+        let mut f = || {
+            n += 1;
+        };
+        f();
+    }
 }

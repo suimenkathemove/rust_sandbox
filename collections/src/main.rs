86ce0efe48d641dda244e775b96894ea1df4c61f
suimenkathemove <yoshiharu2580@gmail.com>
2023-09-10 15:10:28 +0900

    add map to collections

--- collections/src/main.rs
+++ collections/src/main.rs
@@ -1,8 +1,8 @@
 //! # Collections
 //!
-//! シーケンス（sequence）、マップ（map）、セット（set）のデータ構造を提供する
-//! マップには、HashMap、BTreeMapがある
+//! シーケンス（sequence）、マップ（map）、セット（set）のデータ構造を提供する。
 
+mod map;
 mod sequence;
 mod set;
 

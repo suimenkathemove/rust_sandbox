5e7f3dfbf0d8ede89cbcc6a4b3098efa0b9b7b81
suimenkathemove <yoshiharu2580@gmail.com>
2023-09-16 15:41:34 +0900

    refactor

--- asynchronous/src/main.rs
+++
@@ -1,8 +0,0 @@
-#![allow(dead_code, unused_mut, unused_variables)]
-
-mod structs;
-mod tuple_structs;
-
-fn main() {
-    structs::func();
-}
--- structs/src/lib.rs
+++ structs/src/lib.rs
@@ -2,6 +2,5 @@
 //!
 //! ある型に対して、メソッド、型関連関数、型関連定数を定義することができる
 
-fn main() {
-    println!("Hello, world!");
-}
+mod structs;
+mod tuple_structs;

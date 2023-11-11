537430c89aa068ee20432c73d52565548ee9fe8e
suimenkathemove <yoshiharu2580@gmail.com>
2023-11-12 04:08:01 +0900

    add crate to modules

--- modules/src/main.rs
+++ modules/src/main.rs
@@ -1,3 +1,19 @@
+//! ## crate クレート
+//!
+//! crateとは、crate rootとその依存ファイル(modules)のこと。
+//! crate rootには、binary crateのcrate rootであるmain.rs、library crateのcrate rootであるlib.rsがある。
+//! binary crateはそれ単体で実行できる。library crateは他のbinary crateやlibrary crateから利用するためのもの。
+//!
+//! packageとは、crateとCargo.tomlなどのファイルを含めたもの。
+//!
+//! moduleとは、crate内でさらに機能を分割したもの。
+//!
+//! - package
+//!   - crate
+//!     - crate root
+//!     - modules
+//!   - Cargo.toml
+
 fn main() {
     println!("Hello, world!");
 }

2a0c32a29e8c481094557ce56761da087182233b
suimenkathemove <yoshiharu2580@gmail.com>
2023-11-13 20:58:32 +0900

    add to document

--- document/src/main.rs
+++ document/src/main.rs
@@ -1,3 +1,17 @@
+//! # document
+//!
+//! ソースコード中にdocumentation commentを書くと、それを元にドキュメントを生成できる。
+//! documentation commentにはmarkdownを用いることができる。
+//! documentation commentを生成するには`cargo doc`を実行する。
+//!
+//! ## crateやmodule
+//!
+//! crateやmoduleのドキュメントは、`//!`の後に書き、ファイルの先頭に書く。
+
+mod module {
+    //! インラインのモジュールにドキュメントを書く場合はこのように書く。
+}
+
 fn main() {
     println!("Hello, world!");
 }

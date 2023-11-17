073008908caf79bf2b40d7715e009ab9792a8868
suimenkathemove <yoshiharu2580@gmail.com>
2023-11-18 01:06:57 +0900

    add to trait_object

--- traits/src/trait_object.rs
+++ traits/src/trait_object.rs
@@ -7,6 +7,9 @@
 //!
 //! ジェネリックな型引数は一度に1つの具体型にしか置き換えられないのに対して、トレイトオブジェクトは実行時に複数の具体型で埋めることができる。
 //!
+//! トレイトオブジェクトはファットポインタであり、1つ目のポインタは実装先の型を参照し、2つ目のポインタは関数テーブルへのポインタを参照する。
+//! 関数テーブルとは、トレイトの各メソッドの実装を指すポインタの集合のこと。
+//!
 //! ## 静的ディスパッチ
 //!
 //! コンパイル時に、トレイトのメソッドの呼び出し先が決定されること。
@@ -27,3 +30,44 @@
 //! トレイトは、トレイト内で定義されているメソッド全てに以下の特性があれば、オブジェクト安全になる。
 //! - 戻り値の型が`Self`でない
 //! - ジェネリックな型引数がない
+
+mod error {
+    use std::{error::Error, fmt::Display};
+
+    #[derive(Debug)]
+    struct ErrorA;
+
+    impl Display for ErrorA {
+        fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
+            write!(f, "ErrorA")
+        }
+    }
+
+    impl Error for ErrorA {}
+
+    #[derive(Debug)]
+    struct ErrorB;
+
+    impl Display for ErrorB {
+        fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
+            write!(f, "ErrorB")
+        }
+    }
+
+    impl Error for ErrorB {}
+
+    fn error_a() -> Result<(), ErrorA> {
+        Err(ErrorA)
+    }
+
+    fn error_b() -> Result<(), ErrorB> {
+        Err(ErrorB)
+    }
+
+    fn error_a_b() -> Result<(), Box<dyn Error>> {
+        // ?でBox型に変換できている
+        error_a()?;
+        error_b()?;
+        Ok(())
+    }
+}

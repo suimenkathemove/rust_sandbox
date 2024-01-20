b2fe0d87bcb18911daa41d4fcefc9049bd5f5480
suimenkathemove <yoshiharu2580@gmail.com>
2024-01-21 00:45:18 +0900

    add london_school

--- tests/src/unit_testing/e_commerce_website.rs
+++ tests/src/unit_testing/e_commerce_website.rs
@@ -2,13 +2,24 @@
 
 use std::collections::HashMap;
 
-#[derive(Clone, Copy, PartialEq, Eq, Hash)]
+#[derive(Debug, Clone, Copy, PartialEq, Eq, Hash)]
 enum Product {
     Shampoo,
     Book,
 }
 
 type Quantity = u32;
+
+#[cfg_attr(test, mockall::automock)]
+trait IStore {
+    fn get_inventory(&self, product: Product) -> Quantity;
+
+    fn has_enough_inventory(&self, product: Product, quantity: Quantity) -> bool;
+
+    fn add_inventory(&mut self, product: Product, quantity: Quantity);
+
+    fn remove_inventory(&mut self, product: Product, quantity: Quantity) -> bool;
+}
 
 struct Store {
     inventories: HashMap<Product, Quantity>,
@@ -20,7 +31,9 @@ impl Store {
             inventories: HashMap::new(),
         }
     }
+}
 
+impl IStore for Store {
     fn get_inventory(&self, product: Product) -> Quantity {
         *self.inventories.get(&product).unwrap_or(&0)
     }
@@ -34,6 +47,18 @@ impl Store {
         let new_inventory = inventory + quantity;
         self.inventories.insert(product, new_inventory);
     }
+
+    fn remove_inventory(&mut self, product: Product, quantity: Quantity) -> bool {
+        if !self.has_enough_inventory(product, quantity) {
+            return false;
+        }
+
+        let inventory = self.get_inventory(product);
+        let new_inventory = inventory - quantity;
+        self.inventories.insert(product, new_inventory);
+
+        true
+    }
 }
 
 struct Customer;
@@ -43,16 +68,8 @@ impl Customer {
         Self
     }
 
-    fn purchase(&self, store: &mut Store, product: Product, quantity: Quantity) -> bool {
-        if !store.has_enough_inventory(product, quantity) {
-            return false;
-        }
-
-        let inventory = store.get_inventory(product);
-        let new_inventory = inventory - quantity;
-        store.inventories.insert(product, new_inventory);
-
-        true
+    fn purchase(&self, store: &mut impl IStore, product: Product, quantity: Quantity) -> bool {
+        store.remove_inventory(product, quantity)
     }
 }
 
@@ -87,5 +104,36 @@ mod detroit_school {
 
 #[cfg(test)]
 mod london_school {
-    // TODO
+    use super::*;
+    use mockall::predicate::eq;
+
+    #[test]
+    fn purchase_succeeds_when_enough_inventory() {
+        let mut store_mock = MockIStore::new();
+        store_mock
+            .expect_remove_inventory()
+            .with(eq(Product::Shampoo), eq(5))
+            .times(1)
+            .returning(|_, _| true);
+        let customer = Customer::new();
+
+        let success = customer.purchase(&mut store_mock, Product::Shampoo, 5);
+
+        assert_eq!(true, success);
+    }
+
+    #[test]
+    fn purchase_fails_when_not_enough_inventory() {
+        let mut store_mock = MockIStore::new();
+        store_mock
+            .expect_remove_inventory()
+            .with(eq(Product::Shampoo), eq(15))
+            .times(1)
+            .returning(|_, _| false);
+        let customer = Customer::new();
+
+        let success = customer.purchase(&mut store_mock, Product::Shampoo, 15);
+
+        assert_eq!(false, success);
+    }
 }
